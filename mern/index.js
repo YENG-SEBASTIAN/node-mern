cec89e5d70a9decc008ae9f8edbab6082497f90a
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-01-13 07:36:37 +0000

    server

---
+++ mern/index.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://yengsebastian1:Yeng123@cluster0.zuxqswp.mongodb.net/?retryWrites=true&w=majority");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world! Welcome to my first node server")
+})
+
+app.listen(4000, () => console.log("App is running on port 4000..."))
