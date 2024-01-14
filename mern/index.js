77ffade2193175b5f148b0d4d40868de0a6cccdf
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-01-14 18:24:13 +0000

    middleware

--- mern/index.js
+++ mern/index.js
@@ -2,9 +2,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://yengsebastian1:Yeng123@cluster0.zuxqswp.mongodb.net/mernAppDB?retryWrites=true&w=majority");
+mongoose.connect(
+    "mongodb+srv://yengsebastian1:Yeng123@cluster0.zuxqswp.mongodb.net/mernAppDB?retryWrites=true&w=majority"
+).then(
+    () => console.log("\n Database connected successful.")
+).catch(
+    (err) => console.log("\n Database connection failed. ", err)
+)
 
 const app = express();
+
+//middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello world! Welcome to my first node server")
